166230d3b341f3b60c3e00bcfd3750937c5f7ca6
AYAS-PC\aya92 <aya.angel9099@gmail.com>
2022-10-12 13:35:35 +0300

    design+database tables

---
+++ database/migrations/2022_10_11_180558_create_signs_and_signals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('signs_and_signals', function (Blueprint $table) {
+            $table->id();
+            $table->string('sectionName');
+            $table->string('signName');
+            $table->string('img');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('signs_and_signals');
+    }
+};
---
+++ database/migrations/2022_10_12_100517_create_exams_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->id();
+            $table->string('examType');
+            $table->string('question');
+            $table->string('answer1');
+            $table->string('answer2');
+            $table->string('answer3');
+            $table->string('answer4');
+            $table->string('correctAnswer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exams');
+    }
+};
