0a3ae00530cead03c0273d76bbd64e7b2b727c70
AYAS-PC\aya92 <aya.angel9099@gmail.com>
2022-10-17 22:41:26 +0300

    ex

--- resources/views/in/signsAndSignals.blade.php
+++ resources/views/in/signsAndSignals.blade.php
@@ -10,25 +10,24 @@
 <!-- Page Header End -->
 
 <div class="container-xxl py-10">
-<div class="container">
-    <div class="row g-4 justify-content-center" >
-        <div class="col-lg-10 my-6 mb-0 wow fadeInUp">
-    <div class="text-center mx-auto mb-5 wow fadeInUp" style="max-width: 500px;">
-        <h5 class="text-primary text-uppercase mb-2">Click on the section you want to view:-</h5>
+    <div class="container">
+        <div class="row g-4 justify-content-center" >
+            <div class="col-lg-10 my-6 mb-0 wow fadeInUp">
+                <div class="text-center mx-auto mb-5 wow fadeInUp" style="max-width: 500px;">
+                    <h5 class="text-primary text-uppercase mb-2">Click on the section you want to view:-</h5>
+                </div>
+                <div class="row row-cols-3 text-center">
+                    <!-- loop0 here -->
+                    @foreach ($sectionName as $Name)
+
+                    <h5><a href="#{{ $Name->sectionName }}" class="col btn btn-primary w-100 py-3">{{ $Name->sectionName }}</a></h5>
+                
+                    @endforeach
+
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="row row-cols-3 text-center">
-        <!-- loop0 here -->
-        @foreach ($sectionName as $Name)
-
-        <h5><a href="#{{ $Name->sectionName }}" class="col btn btn-primary w-100 py-3">{{ $Name->sectionName }}</a></h5>
-       
-        @endforeach
-
-    </div>
-
-</div>
-</div>
-</div>
 </div>
 
 <!-- loop1 here -->
